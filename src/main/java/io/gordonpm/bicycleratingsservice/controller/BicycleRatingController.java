7952d6d4fece489ad265b6d3f2462861897cbf19
gordonpm <gordonm0815@gmail.com>
2022-08-23 13:45:46 -0700

    Added endpoint for getting user rating for a given user

--- src/main/java/io/gordonpm/bicycleratingsservice/controller/BicycleRatingController.java
+++ src/main/java/io/gordonpm/bicycleratingsservice/controller/BicycleRatingController.java
@@ -1,9 +1,13 @@
 package io.gordonpm.bicycleratingsservice.controller;
 
 import io.gordonpm.bicycleratingsservice.models.BicycleRating;
+import io.gordonpm.bicycleratingsservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/bicycleratings")
@@ -14,5 +18,15 @@ public class BicycleRatingController {
         return new BicycleRating(bicycleId, 4);
     }
 
+    @RequestMapping("/users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId) {
+        List<BicycleRating> ratings = Arrays.asList(
+                new BicycleRating("100", 4),
+                new BicycleRating("200", 3)
+        );
 
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
+    }
 }
